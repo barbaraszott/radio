adaad6e20300ecbe35868fa70a81925aa3a4cf60
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-23 15:28:04 +0200

    Refactor in CurrentlyPlaying - remove unnecessary variables

--- src/CurrentlyPlaying.js
+++ src/CurrentlyPlaying.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 
 class CurrentlyPlaying extends Component {
 	render() {
-		const isPlaying = this.props.playing !== null;
-		const show = { display: isPlaying ? 'block' : 'none' };
-		const station = this.props.playing;
-
 		return (
 			<React.Fragment>
 				<span className="currently-playing-header">currently playing</span>
-				<span className="currently-playing-station">{station}</span>
+				<span className="currently-playing-station">{this.props.playing}</span>
 			</React.Fragment>
 		);
 	}

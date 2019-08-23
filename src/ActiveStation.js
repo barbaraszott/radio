4d2757ac8fe7c288888c92ed1c14422b496af573
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-23 15:12:38 +0200

    Create ActiveStation component
    Component is shown underneath station that was chosen

---
+++ src/ActiveStation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
+
+class ActiveStation extends Component {
+	render() {
+		return (
+			<div className="active-station">
+				<div className="active-station-cover" />
+			</div>
+		);
+	}
+}
+
+export default ActiveStation;

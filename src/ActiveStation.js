9f9ad4545d3958a6c1ccbe8ca2b5006155eb368b
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-23 16:30:36 +0200

    Add volume buttons

--- src/ActiveStation.js
+++ src/ActiveStation.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types'
+import plus from './imgs/plus.png';
+import minus from './imgs/minus.png';
 
 class ActiveStation extends Component {
 	render() {
 		return (
 			<div className="active-station">
+				<div className="active-station-volume">
+					<img src={minus} alt="Turn down the volume" />
+				</div>
 				<div className="active-station-cover" />
+				<div className="active-station-volume">
+					<img src={plus} alt="Turn up the volume" />
+				</div>
 			</div>
 		);
 	}

77044db926570ecd6b020e0e095ed6511c01851f
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-24 14:17:08 +0200

    API call simulation

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import Station from './Station';
 import CurrentlyPlaying from './CurrentlyPlaying';
 import backArrow from './imgs/back-arrow.png';
 import switchBtn from './imgs/switch.png';
-import { stations } from './stations';
+import { getStations } from './stations';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			currentStation : null
+			currentStation : null,
+			stations       : [],
+			isLoading      : true,
+			failed         : false
 		};
+	}
+
+	componentDidMount() {
+		getStations()
+			.then((value) => {
+				console.log('Data received!');
+				this.setState({
+					stations  : value,
+					isLoading : false
+				});
+			})
+			.catch((error) => {
+				console.log(error.message);
+
+				this.setState({
+					isLoading : false,
+					failed    : true
+				});
+			});
+
+		console.log('API called, waiting for data');
 	}
 
 	switchOff = () => {
@@ -40,19 +64,25 @@ class App extends React.Component {
 						<img src={switchBtn} alt="Switch on/off" />
 					</div>
 				</div>
-				<ul className="radio-stations">
-					{stations.map((station, i) => (
-						<li key={i}>
-							<Station
-								name={station.name}
-								frequency={station.frequency}
-								cover={station.cover}
-								onStationClick={this.showStation}
-								isCurrentlyPlaying={this.state.currentStation === station.name}
-							/>
-						</li>
-					))}
-				</ul>
+
+				<div className="radio-stations">
+					{this.state.isLoading && <p className="status-msg">Loading...</p>}
+					{this.state.failed && <p className="status-msg">Sorry! No radio today :(</p>}
+					<ul className="radio-stations-list">
+						{this.state.stations.map((station, i) => (
+							<li key={i}>
+								<Station
+									name={station.name}
+									frequency={station.frequency}
+									cover={station.cover}
+									onStationClick={this.showStation}
+									isCurrentlyPlaying={this.state.currentStation === station.name}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
+
 				<div className="currently-playing">
 					{this.state.currentStation && <CurrentlyPlaying playing={this.state.currentStation} />}
 				</div>
--- src/stations.js
+++ src/stations.js
@@ -1,4 +1,4 @@
-export const stations = [
+const APIResponse = [
 	{ name: 'Putin FM', frequency: '66,6' },
 	{ name: 'Dribbble FM', frequency: '101,2' },
 	{ name: 'Doge FM', frequency: '99,4' },
@@ -9,3 +9,15 @@ export const stations = [
 	},
 	{ name: 'Maximum FM', frequency: '142,2' }
 ];
+
+export const getStations = () => {
+	console.log(`Let's call API!`);
+
+	const mockedAPICall = new Promise((resolve, reject) => {
+		// this reject is just for testing error handling
+		// reject(new Error('ERROR!'));
+		setTimeout(() => resolve(APIResponse), 500);
+	});
+
+	return mockedAPICall;
+};

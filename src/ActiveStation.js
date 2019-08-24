ca1109741ecd5c6fe5df61bdcb38676be49e6faf
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-24 12:27:24 +0200

    Create default cover, add background-image with cover in ActiveStation, add PropTypes

--- src/ActiveStation.js
+++ src/ActiveStation.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import plus from './imgs/plus.png';
 import minus from './imgs/minus.png';
+import defaultCover from './imgs/station-cover.png';
 
 class ActiveStation extends Component {
+	static defaultProps = {
+		cover : defaultCover
+	};
+
 	render() {
+		const backgroundImg = { backgroundImage: `url(${this.props.cover})` };
+
 		return (
 			<div className="active-station">
 				<div className="active-station-volume">
 					<img src={minus} alt="Turn down the volume" />
 				</div>
-				<div className="active-station-cover" />
+				<div className="active-station-cover" style={backgroundImg} />
 				<div className="active-station-volume">
 					<img src={plus} alt="Turn up the volume" />
 				</div>
@@ -18,5 +25,9 @@ class ActiveStation extends Component {
 		);
 	}
 }
+
+ActiveStation.propTypes = {
+	cover : PropTypes.string
+};
 
 export default ActiveStation;

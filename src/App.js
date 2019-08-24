2376fd6a3b05af32066d586a68f0365e4d3d29dc
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-24 12:51:48 +0200

    Extract stations' array to separate file

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import Station from './Station';
 import CurrentlyPlaying from './CurrentlyPlaying';
 import backArrow from './imgs/back-arrow.png';
 import switchBtn from './imgs/switch.png';
-
-const stations = [
-	{ name: 'Putin FM', frequency: '66,6' },
-	{ name: 'Dribbble FM', frequency: '101,2' },
-	{ name: 'Doge FM', frequency: '99,4' },
-	{
-		name      : 'Ballads FM',
-		frequency : '87,1',
-		cover     : 'https://coolsilh.com/png-256/2167964-moon_001.png'
-	},
-	{ name: 'Maximum FM', frequency: '142,2' }
-];
+import { stations } from './stations';
 
 class App extends React.Component {
 	constructor(props) {
---
+++ src/stations.js
@@ -0,0 +1,11 @@
+export const stations = [
+	{ name: 'Putin FM', frequency: '66,6' },
+	{ name: 'Dribbble FM', frequency: '101,2' },
+	{ name: 'Doge FM', frequency: '99,4' },
+	{
+		name      : 'Ballads FM',
+		frequency : '87,1',
+		cover     : 'https://coolsilh.com/png-256/2167964-moon_001.png'
+	},
+	{ name: 'Maximum FM', frequency: '142,2' }
+];

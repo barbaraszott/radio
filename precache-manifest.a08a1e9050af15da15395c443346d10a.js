8fdc0b9dbf60143d27cc66ce83ffaee5d3866b03
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-25 10:59:09 +0200

    Updates

---
+++ precache-manifest.a08a1e9050af15da15395c443346d10a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08bbd4003d4cde88470a3e29c79099b5",
+    "url": "/radio/index.html"
+  },
+  {
+    "revision": "d5d466a40b51be268a40",
+    "url": "/radio/static/css/main.52798c13.chunk.css"
+  },
+  {
+    "revision": "c2c8ed565ff972bfa7a9",
+    "url": "/radio/static/js/2.90821591.chunk.js"
+  },
+  {
+    "revision": "d5d466a40b51be268a40",
+    "url": "/radio/static/js/main.fb65b5e9.chunk.js"
+  },
+  {
+    "revision": "dacd3a8578f910f65ae5",
+    "url": "/radio/static/js/runtime~main.62d18162.js"
+  },
+  {
+    "revision": "8b358c45871564e51ffdb25e09b26eae",
+    "url": "/radio/static/media/back-arrow.8b358c45.png"
+  },
+  {
+    "revision": "9d1af8441c33e5a39a802a8a1b4bbbbb",
+    "url": "/radio/static/media/minus.9d1af844.png"
+  },
+  {
+    "revision": "10db4c258b064eea7c0623e26b228f80",
+    "url": "/radio/static/media/plus.10db4c25.png"
+  },
+  {
+    "revision": "d84344b8e16b707989df0a663a5231c6",
+    "url": "/radio/static/media/station-cover.d84344b8.png"
+  },
+  {
+    "revision": "3a0c76ad0e075df488bc636898762f25",
+    "url": "/radio/static/media/switch.3a0c76ad.png"
+  }
+]);
